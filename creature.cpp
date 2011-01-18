0d72ad2c7f0f5879bdc17cbb123683ef8f4d8f82
Samuel Bryner <samuelbryner@gmx.ch>
2011-01-18 16:50:23 +0000

    creature added

---
+++ creature.cpp
@@ -0,0 +1,114 @@
+#include "creature.hpp"
+
+//------------------------------------------------------------------------------
+Creature::Creature()
+ : shape(50,50,30,40)
+{
+
+};
+//------------------------------------------------------------------------------
+void
+Creature::move(double sec, TileMap& map)
+{
+	if(this->health > 0) /// still alive?
+	{
+		/// Obey gravity. It's the law!
+		speed.y += GRAVITY*sec;
+
+		vector2<double> newpos = shape.center + speed*sec;
+
+		/// horizontal bounding
+		if (newpos.x + shape.extend.x >= map.getPixelWidth())
+		{
+			speed.x  = 0;
+			newpos.x = map.getPixelWidth() - shape.extend.x;
+		}
+		else if (newpos.x < shape.extend.x)
+		{
+			speed.x  = 0;
+			newpos.x = shape.extend.x;
+		}
+
+		/// vertical bounding
+		if (newpos.y + shape.extend.y >= map.getPixelHeight())
+		{
+			speed.y  = 0;
+			newpos.y = map.getPixelHeight() - shape.extend.y;
+			touching = true;
+		}
+		else if (newpos.y < shape.extend.y)
+		{
+			speed.y  = 0;
+			newpos.y = shape.extend.y;
+		}
+
+		/// collision with map
+
+		/// horizontal
+		if (speed.x != 0 and newpos.x - shape.center.x != 0) ///< horiz movement?
+		{
+			CBoxEdge<double> tmpedge = shape.edgeX(speed.x);
+
+			/// otherwise we get collisions due to small errors:
+			tmpedge.p1.y += 0.1;
+			tmpedge.p2.y -= 0.1;
+
+			double col_dist = map.getDistToNearestBlock(tmpedge, newpos.x-shape.center.x, 1, 1);
+
+			if (Abs(col_dist) < Abs(newpos.x-shape.center.x))
+			{
+				newpos.x = shape.center.x + col_dist;
+				speed.x  = 0;
+			}
+
+			shape.center.x = newpos.x;
+		}
+
+		/// vertical
+		if (speed.y != 0 and newpos.y-shape.center.y != 0) ///< vertical movement?
+		{
+			CBoxEdge<double> tmpedge = shape.edgeY(speed.y);
+
+			/// again, to counter small errors:
+			tmpedge.p1.x += 0.1;
+			tmpedge.p2.y -= 0.1;
+
+			double col_dist = map.getDistToNearestBlock(tmpedge, newpos.y-shape.center.y, 2, 1);
+
+			if (Abs(col_dist) < Abs(newpos.y-shape.center.y))
+			{
+				if (col_dist >= 0) touching = true;
+
+				newpos.y = shape.center.y + col_dist;
+				speed.y  = 0;
+			}
+		}
+
+		shape.center = newpos;
+	}
+};
+//------------------------------------------------------------------------------
+void
+Creature::jump(double s)
+{
+	if (this->touching and s > 0 and speed.y <= 0)
+	{
+		speed.y  = -s;
+		touching = false;
+	}
+};
+//------------------------------------------------------------------------------
+void
+Creature::swap_momentum(Creature& c)
+{
+	swap(this->speed, c.speed);
+};
+//------------------------------------------------------------------------------
+void
+Creature::swap_position(Creature& c)
+{
+	swap(this->shape.center, c.shape.center);
+};
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
---
+++ creature.hpp
@@ -0,0 +1,24 @@
+#ifndef CREATURE_HPP_INCLUDED
+#define CREATURE_HPP_INCLUDED
+
+#include "a10_common.hpp"
+#include "tile_map.hpp"
+
+struct Creature
+{
+	CBox<double>    shape;
+	vector2<double> speed;
+	bool touching;
+	double health;
+
+	Creature();
+
+	void move(double sec, TileMap& map);
+
+	inline void setXSpeed(double s){this->speed.x = s;}
+	void jump(double s);
+	void swap_momentum(Creature& c);
+	void swap_position(Creature& c);
+};
+
+#endif // CREATURE_HPP_INCLUDED
